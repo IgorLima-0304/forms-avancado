a83f45e8e0b421b3c1bf6a6f69a77a6c121347d4
Igor Lima <igorlimafac@gmail.com>
2024-02-05 18:06:10 -0300

    adicionando visual

---
+++ forms-avancado/src/App.tsx
@@ -0,0 +1,31 @@
+import "./styles/global.css";
+
+export function App() {
+  return (
+    <main className="h-screen bg-zinc-50 flex items-center justify-center">
+      <form action="flex flex-col">
+        <div className="flex flex-col gap-4">
+          <label htmlFor="">E-mail</label>
+          <input type="email" name="email" />
+        </div>
+        <div className = "flex flex-col gap-1">
+          <label htmlFor=""> Senha </label>
+          <input
+
+           type="password" 
+           name="password" 
+           className = "border border-zinc-200 shadow-sn rounded"
+           />
+        </div>
+        
+        <button 
+          type ="submit"
+          className = "bg-emerald-500 rounded font-semibold text-white h-10 hover: hover:bg-emerald-600 w-40"
+
+          >
+          Salvar  
+        </button>
+      </form>
+    </main>
+  );
+}
